c777d84114fcf6c43ef5bdcbdbe073cd4d245488
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-27 21:37:10 +0100

    Update gemini_pdf_chat.py

--- gemini_pdf_chat.py
+++ gemini_pdf_chat.py
@@ -1,1 +1,22 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple pdfs", page_icon=":books:")
+    st.header("Chat with multiple PDFs :books:")
+
+    user_question = st.text_input("Ask a question about your documents:")
+    st.subheader("Your documents")
+    pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    st.button("Process")
+    
+
+if __name__ == '__main__':
+    main()
