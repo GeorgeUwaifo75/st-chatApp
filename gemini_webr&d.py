012b644e7e054e3ca1704f0a1193a880c731ddc8
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-02 02:12:54 +0100

    Update gemini_webr&d.py

--- gemini_webr&d.py
+++ gemini_webr&d.py
@@ -2,6 +2,7 @@ import streamlit as st
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders import UnstructuredURLLoader
 
+st.title("URL load and Analyzer")
 loader = UnstructuredURLLoader(
     urls = [
         "https://www.moneycontrol.com/news/india/two-women-die-6-taken-ill-after-consuming-mango-kernel-gruel-in-odisha-12856689.html",
@@ -10,4 +11,4 @@ loader = UnstructuredURLLoader(
     ] 
 )
 data = loader.load()
-st.write(len(data))
+st.write("The number of URLs: "len(data))
