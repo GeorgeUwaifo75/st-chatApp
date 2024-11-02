198f64e49891989c3454a954dd5c043af3c78525
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-02 02:01:51 +0100

    Update gemini_webr&d.py

--- gemini_webr&d.py
+++ gemini_webr&d.py
@@ -1,1 +1,11 @@
 import streamlit as st
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredURLLoader
+
+loader = UnstructuredURLLoader(
+    urls = [
+        "https://www.moneycontrol.com/news/india/two-women-die-6-taken-ill-after-consuming-mango-kernel-gruel-in-odisha-12856689.html",
+        "https://www.nimbleway.com/blog/use-serp-api-to-boost-rankings-and-explore-markets"
+
+    ] 
+)
