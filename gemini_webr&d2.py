18e3a23e94641e4d2eb7b83d4681f48e65874a58
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-04 01:16:34 +0100

    Update gemini_webr&d2.py

--- gemini_webr&d2.py
+++ gemini_webr&d2.py
@@ -143,9 +143,9 @@ def main():
         if process_url:    
             with st.spinner("Processing"):
                 st.write("T2:",text2[:100])
-                if type_v =1:
+                if type_v ==1:
                     raw_text = get_web_text()
-                elif type_v=2:  
+                elif type_v==2:  
                     raw_text = text2
 
                 
