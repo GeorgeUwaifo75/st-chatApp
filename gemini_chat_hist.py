6d3cbdca096aac1a47dacfe1359af4784c9b47aa
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-11 12:34:17 +0100

    Create gemini_chat_hist.py

---
+++ gemini_chat_hist.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+st.write("AI Chat with History")
+
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
