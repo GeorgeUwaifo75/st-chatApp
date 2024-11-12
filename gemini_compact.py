b17f4e51f16863c27c85f4d99914098fd2a520a0
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-12 15:01:03 +0100

    Update gemini_compact.py

--- gemini_compact.py
+++ gemini_compact.py
@@ -107,7 +107,7 @@ def handle_userinput2(question):
         st.markdown(question)
 
     # Answer the question
-#    answer, doc_source = generate_answer(user_question)
+    answer, doc_source = generate_answer(question)
 #    with st.chat_message("assistant"):
 #        st.write(answer)
     # Append assistant answer to history
