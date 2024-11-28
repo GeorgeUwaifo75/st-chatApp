1a022d23052c81b018f58e41e601552998dced57
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-28 12:14:53 +0100

    Update bugatti.py

--- bugatti.py
+++ bugatti.py
@@ -218,7 +218,7 @@ def main():
                 st.markdown(""" div.stButton > button:first-child {
                 background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
                 }
-                “”", unsafe_allow_html=True)
+                """, unsafe_allow_html=True)
 
             
                 process_url = st.button("Process Docs")
