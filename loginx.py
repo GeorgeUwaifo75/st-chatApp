1b46cefced6cbccd9714e897a25fb16ddd566060
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-28 13:42:05 +0100

    Update loginx.py

--- loginx.py
+++ loginx.py
@@ -18,16 +18,16 @@ if not st.session_state.user_state['logged_in']:
     submit = st.button('Login')
 
 # Check if user is logged in
-    if submit and st.session_state.user_state['logged_in'] == False:
-        if username == 'admin' and password == '1234':
-            st.session_state.user_state['username'] = username
-            st.session_state.user_state['password'] = password
-            st.session_state.user_state['logged_in'] = True
-            st.write('You are logged in')
-            st.rerun()
-        else:
-            st.warning('Invalid username or password')
-    
-    elif st.session_state.user_state['logged_in']:
-        st.write('Welcome to the app')
-        st.write('You are logged in as:', st.session_state.user_state['username'])
+if submit and st.session_state.user_state['logged_in'] == False:
+    if username == 'admin' and password == '1234':
+        st.session_state.user_state['username'] = username
+        st.session_state.user_state['password'] = password
+        st.session_state.user_state['logged_in'] = True
+        st.write('You are logged in')
+        st.rerun()
+    else:
+        st.warning('Invalid username or password')
+
+elif st.session_state.user_state['logged_in']:
+    st.write('Welcome to the app')
+    st.write('You are logged in as:', st.session_state.user_state['username'])
