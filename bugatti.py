c9d74ce62383e1b5dd4f38b0b0263f159676d0db
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-28 12:51:09 +0100

    Update bugatti.py

--- bugatti.py
+++ bugatti.py
@@ -174,7 +174,7 @@ def main():
     load_dotenv()
     
     st.set_page_config(page_title="GiTeksol Document Assistant", page_icon=":books:")
-    st.write("This is :blue[test]")
+    #st.write("This is :blue[test]")
     
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -182,7 +182,7 @@ def main():
         st.session_state.chat_history = None
 
    
-    st.header("GiTeksol Document Assistant [*GDA*]")
+    st.header("GiTeksol Document Assistant [*:blue[GDA]*]")
 
     user_question = st.text_input("Ask a question about your documents:")
 
