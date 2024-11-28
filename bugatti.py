49c1f701c9979d255dffb9b0bb2bf13e180dc555
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-28 11:57:46 +0100

    Update bugatti.py

--- bugatti.py
+++ bugatti.py
@@ -191,7 +191,14 @@ def main():
     if user_question:
         handle_userinput(user_question)
         
-       
+    st.markdown("""
+    <style>
+        [data-testid=stSidebar] {
+            background-color: #ff000050;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    
     st.sidebar.title("Source of Doc.")
     doc_type = st.sidebar.selectbox("Pick Doc Source", ("Doc Types","URL", "PDF", "IvieAI"))
 
