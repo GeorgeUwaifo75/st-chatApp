5a548c325b33cf0612b0a7cffa889393ae3ff93b
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-28 12:25:23 +0100

    Update bugatti.py

--- bugatti.py
+++ bugatti.py
@@ -30,6 +30,31 @@ json_data = response.content
 
 urls = []
 
+st.markdown(""" 
+div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+}
+.css-2trqyj:focus:not(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:active){
+background-color: #0066cc;
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+""", unsafe_allow_html=True)
+
 
 #Upload IvieAI dataset
 def upload_ivieAi():
