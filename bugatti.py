302fddbaa91b57d20094fe23fd4f3b0bb1e9f2c5
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-28 12:06:25 +0100

    Update bugatti.py

--- bugatti.py
+++ bugatti.py
@@ -194,7 +194,7 @@ def main():
     st.markdown("""
     <style>
         [data-testid=stSidebar] {
-            background-color: #80DAEB;
+            background-color: #00CCFF;
         }
     </style>
     """, unsafe_allow_html=True)
