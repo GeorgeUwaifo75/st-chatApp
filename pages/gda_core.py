7be1e9f5c7775e3b01526ed40b89d69e34f306c3
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-30 00:35:58 +0100

    Update gda_core.py

--- pages/gda_core.py
+++ pages/gda_core.py
@@ -157,7 +157,7 @@ def get_conversation_chain(vectorstore):
     #endpoint_url="mistralai/Mistral-7B-Instruct-v0.2/",temperature=0.65, max_length=512)
     #endpoint_url="Qwen/QwQ-32B-Preview",temperature=0.65, max_length=512)
     endpoint_url="meta-llama/Llama-3.1-8B-Instruct/",temperature=0.3, max_length=512)
-   
+    st.write("[Llama]")
     
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
