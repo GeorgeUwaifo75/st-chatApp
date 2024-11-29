e9b5a29cff8ac289382a77cb84cad7691f85afad
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-29 11:40:43 +0100

    Update loginx.py

--- loginx.py
+++ loginx.py
@@ -6,9 +6,6 @@ import requests as rs
 #from st_pages import hide_pages
 from time import sleep
 
-from streamlit import source_util
-from streamlit.runtime.scriptrunner import get_script_run_ctx
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,53 +13,7 @@ load_dotenv()
 #The tutorial page
 #https://oguzhari.medium.com/making-user-login-application-with-streamlit-91ce5e598f23
 
-st.title('Amazing User Login App')
-
-#********************************
-
-page = "pages/bugatti.py" # MODIFY TO YOUR PAGE
-
-ctx = get_script_run_ctx()
-ctx_main_script = ""
-if ctx:
-  ctx_main_script = ctx.main_script_path
-
-st.write("**Main Script File**")
-st.text(f"\"{ctx_main_script}\"")
-
-st.write("**Current Working Directory**")
-st.text(f"\"{os.getcwd()}\"")
-
-st.write("**Normalized Current Working Directory**")
-st.text(f"\"{os.path.normpath(os.getcwd())}\"")
-
-st.write("**Main Script Path**")
-main_script_path = os.path.join(os.getcwd(), ctx_main_script)
-st.text(f"\"{main_script_path}\"")
-
-st.write("**Main Script Directory**")
-main_script_directory = os.path.dirname(main_script_path)
-st.text(f"\"{main_script_directory}\"")
-
-st.write("**Normalized Path**")
-page = os.path.normpath(page)
-st.text(f"\"{page}\"")
-
-st.write("**Requested Page**")
-requested_page = os.path.join(main_script_directory, page)
-st.text(f"\"{requested_page}\"")
-
-st.write("**All Pages Page**")
-all_app_pages = list(source_util.get_pages(ctx_main_script).values())
-st.json(all_app_pages, expanded=True)
-
-#st.page_link(page, label="Page Two")
-
-
-
-
-#********************************
-
+st.title('User Login')
 
 #sheet_csv = st.secrets["database_url"]
 #sheet_csv = os.environ.get["database_url"]
@@ -120,7 +71,7 @@ elif st.session_state.user_state['logged_in']:
     st.write('Your fixed user message:', st.session_state.user_state['fixed_user_message'])
     
     sleep(0.5)
-    st.switch_page("pages/page1.py")
+    st.switch_page("pages/bugatti.py")
     #st.page_link("/mount/src/st-chatapp/pages/bugatti.py")
     
     if st.session_state.user_state['user_type'] == 'admin':
