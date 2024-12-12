dd7530c32c0980404481e48ac9ae7eb1dd5905dd
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-12-12 18:41:05 +0100

    Update gda_core.py

--- pages/gda_core.py
+++ pages/gda_core.py
@@ -99,23 +99,6 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 
-def generate_answer2(question):
-    response = st.session_state.conversation({"question": question})
-
-    st.session_state.chat_history = response['chat_history']
-    #st.write(response)  # Return only the answer from the response
-   
-    
-    answer = response.get("answer").split("Helpful Answer:")[-1].strip()
-    explanation = response.get("source_documents", [])
-    doc_source = [d.page_content for d in explanation]
-
-    #st.write("Type of response is: ",type(response))
-    #st.write("Value of response is: ",response)
-
-    dump_resp = json.dumps(response);
-    
-    return answer, doc_source, response
 
 def generate_answer(question):
     response = st.session_state.conversation({"question": question})
