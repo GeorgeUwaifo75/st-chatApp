d721b438f57cc964e2a05c462055758afcbf6032
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-12-27 14:52:53 +0100

    Update chat_chat.py

--- chat_chat.py
+++ chat_chat.py
@@ -15,7 +15,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-prompt = st.chat_input("What is up?"):
+prompt = st.chat_input("What is up?")
 # Add user message to chat history
 st.session_state.messages.append({"role": "user", "content": prompt})
 
