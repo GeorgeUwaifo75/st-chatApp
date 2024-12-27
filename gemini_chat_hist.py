f2f7d640d4ba053a2105a24315ef0e0ea40a8249
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-12-27 14:34:18 +0100

    Update gemini_chat_hist.py

--- gemini_chat_hist.py
+++ gemini_chat_hist.py
@@ -1,38 +1,23 @@
 import streamlit as st
-import random
-import time
+from openai import OpenAI
 
-# Streamed response emulator
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
-
-st.title("Simple chat")
-# Initialize chat history if "messages" not in st.session_state:
-st.session_state.messages = []
-
+st.title("ChatGPT-like clone")
+# Set OpenAI API key
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-# Accept user input if prompt := st.chat_input("What is up?"):
-# Add user message to chat history
-st.session_state.messages.append({"role": "user", "content": prompt})
-
-# Display user message in chat message container with st.chat_message("user"):
-st.chat_message("user")
-st.markdown(prompt)
-
-# Display assistant response in chat message container with st.chat_message("assistant"):
-response = response_generator()
-for line in response:
-    st.write(line)
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
