d6543c467639c8a1279008e993143b528edbf03e
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-01-04 09:14:39 +0100

    Update gda_core.py

--- pages/gda_core.py
+++ pages/gda_core.py
@@ -30,12 +30,10 @@ json_data = response.content
 
 urls = []
 
-def clear_text():
-    st.session_state.my_text = st.session_state.widget
-    #st.session_state.widget = ""
+#def clear_text():
+#    st.session_state.my_text = st.session_state.widget
+#    st.session_state.widget = ""
 
-def clear_text2():
-    st.session_state.widget = ""
 
 #Upload IvieAI dataset
 def upload_ivieAi():
@@ -202,8 +200,8 @@ def main():
     st.header("GiTeksol :green[Document] Assistant [*:blue[GDA]*]")
 
     #display_chat_history()
-    #user_question = st.text_input("Ask a question about your documents:")
-    user_question = st.text_input("Ask a question about your documents:", key='widget', on_change=clear_text)  
+    user_question = st.text_input("Ask a question about your documents:")
+    #user_question = st.text_input("Ask a question about your documents:", key='widget', on_change=clear_text)  
    
     # Ask a question
     if user_question:
